ce61a9445cd2227197a6e7fd86eeec428f39f7d4
zouhir <rajdaouizouhir.pro@gmail.com>
2024-03-03 22:08:15 +0100

    feat: Created cat domain model.

---
+++ composeApp/src/commonMain/kotlin/features/cats/domain/modesl/Cat.kt
@@ -0,0 +1,14 @@
+package features.cats.domain.modesl
+
+data class Cat(
+    val id: String,
+    val url: String,
+    val bread: Bread
+) {
+    data class Bread(
+        val name: String,
+        val lifeSpan: String,
+        val location: String
+    )
+}
+
